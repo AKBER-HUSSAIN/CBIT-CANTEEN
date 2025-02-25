de3ae2a44036b2633ef83aa535862c2029e46c5e
Akber Hussain <036akberhussain@gmail.com>
2025-02-25 12:47:52 +0530

    Added Frontend and Backend Folder

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap styles
+import "animate.css"; // Animations
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css"; // ✅ Correct path
+import App from "./App.jsx";
+import "./styles/global.css"; 
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
